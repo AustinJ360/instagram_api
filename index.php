2563fa6c5c9428572b177fce71ca42b30d09e8a7
Austin Juntra <AustinJuntra@gmail.com>
2015-05-14 08:54:14 -0700

    Milestone 13

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	ini_set('default_socket_timeout', 300);
 	session_start();
 	//Make Constants using define.
-	define('clientID',     'd6577212add54a37995ab7badf09d4d9');
+	define('clientID', 'd6577212add54a37995ab7badf09d4d9');
 	define('clientSecret', '525f73951ee74a2f831178cb8c04320b');
 	define('redirectURI', 'http://localhost/appacademyapi/index.php');
 	define('ImageDirectory', 'pics/');
@@ -23,23 +23,34 @@
 	}
 	//function to get userID cause username doesn't allow us to get picture!
 	function getUserID($userName){
-		$url = 'http://api.instagram.com/v1/users/search?q='.$userName.'&client_id='.clientID;
-		$instagramInfo = connectToInstagram($url);
-		$results = json_decode($instagramInfo, true);
-		return $results['data']['0']['id'];//echoing out userID.	}
+		$url = 'https://api.instagram.com/v1/users/search?q='.$userName.'&client_id='.clientID;
+		$instagramInfo = connectToInstagram($url);//connnecting to Instagram 
+		$results = json_decode($instagramInfo, true);//creating a local variable to decode json infomation.
+		return $results['data'][0]['id'];//echoing out userID.
+	}
 	//function to print out images onto screen
 	function printImages($userID){
-		$url = 'https://api.instagram.com/vl/users/'.$userID.'/media/recent?client_id='.clientID.'&count=5';
+		$url = 'https://api.instagram.com/v1/users/'.$userID.'/media/recent?client_id='.clientID.'&count=5';
 		$instagramInfo = connectToInstagram($url);
 		$results = json_decode($instagramInfo, true);
 		//Parse through the info. one by one.
 		foreach ($results['data'] as $items){
 			$image_url = $items['images']['low_resolution']['url'];//going to go through all of my results and give myself back the URL of those pictures because we want to save it in the PHP server.
-		echo '<img src=" '.$image_url.'"/><br/>';		
+			echo '<img src=" '.$image_url.'"/><br/>';	
+			//calling a function to save that $image_url
+			savePictures($image_url);	
 		}
 	}
+//function to save image to server
+	function savePictures($image_url){
+		echo $image_url .'<br>';
+		$filename = basename($image_url);//the filename is what we are storing. basename is the PHP built in method that we are using to store $image_url
+		echo $filename . '<br>';
+		$destination = ImageDirectory . $filename;//making sure that the image doesn't exist in the storage.
+		file_put_contents($destination, file_get_contents($image_url));//goes and grabs an imagefile and stores is into our sserver/.
+	}
 	if (isset($_GET['code'])){
-		$code = ($_GET['code']);
+		$code = $_GET['code'];
 		$url = 'https://api.instagram.com/oauth/access_token';
 		$access_token_settings = array('client_id' => clientID, 
 									   'client_secret' => clientSecret,
@@ -68,7 +79,7 @@ else{
 	<meta charset="utf-8">
 	<meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale1.0">
-	<title>Austin</title>
+	<title>Jash</title>
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="author" href="human.txt">
 </head>
